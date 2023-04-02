ef3031d07b260e536ac35d510f5a93ef0bbb3138
mayra28mos <mayra28mos@gmail.com>
2023-04-02 13:59:06 +0200

    add protected to enable mass assignment and seller function with relation to seller model

--- backend/app/Models/selleWithdrawalAccount.php
+++ backend/app/Models/selleWithdrawalAccount.php
@@ -8,4 +8,11 @@ use Illuminate\Database\Eloquent\Model;
 class selleWithdrawalAccount extends Model
 {
     use HasFactory;
+
+    protected $guarded = [];
+
+    public function Seller()
+    {
+        return $this->belongsTo(Seller::class);
+    }
 }
