fa7a86bddb2313ff560c5bc6b202e3135838b6e1
mayra28mos <mayra28mos@gmail.com>
2023-04-02 16:37:15 +0200

    add register request

---
+++ backend/app/Http/Requests/RegisterRequest.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace App\Http\Requests;
+
+use Illuminate\Foundation\Http\FormRequest;
+
+class RegisterRequest extends FormRequest
+{
+    /**
+     * Determine if the user is authorized to make this request.
+     */
+    public function authorize(): bool
+    {
+        return true;
+    }
+
+    /**
+     * Get the validation rules that apply to the request.
+     *
+     * @return array<string, \Illuminate\Contracts\Validation\Rule|array|string>
+     */
+    public function rules(): array
+    {
+        return [
+            'name' => 'required',
+            'email' => 'required|email',
+            'email_verified_at'=>'required|same:email' ,
+            'password' => 'required|min:6',     
+            'password_confirm'=>'required|same:password',                                                                
+        ];
+    }
+}
