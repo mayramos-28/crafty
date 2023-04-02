b6d463cd7dfe44ce3b61e8b3334e13c7a8647a5e
mayra28mos <mayra28mos@gmail.com>
2023-04-02 13:24:10 +0200

    add seller table

---
+++ backend/database/migrations/2023_04_02_102208_create_sellers_table.php
@@ -0,0 +1,39 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('sellers', function (Blueprint $table) {
+            $table->id();
+            $table->string('bussnessId');
+            $table->string('bussnessName');
+            $table->string('bussnessType');
+            $table->string('bussnessAddress');
+            $table->string('bussnessPhone');
+            $table->string('bussnessEmail');
+            $table->string('bussnessWebsite');
+            $table->string('bussnessLogo');
+            $table->string('bussnessDescription');
+            $table->unsignedBigInteger('userId')
+                ->references('id')
+                ->on('users')->onDelete('cascade');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('vendors');
+    }
+};
