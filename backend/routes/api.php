ba122660064362e03f099d373292baebb76e409a
mayra28mos <mayra28mos@gmail.com>
2023-04-15 15:29:29 +0200

    añadir grupo de rutas auth con las rutas del login y registro

--- backend/routes/api.php
+++ backend/routes/api.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\AuthController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
@@ -16,6 +17,11 @@ use Illuminate\Support\Facades\Route;
 
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
-});
+ });
 
-Route::post('/register', 'App\Http\Controllers\RegisterController@register');
+//Route::post('/register', 'App\Http\Controllers\RegisterController@register');
+
+Route::group(['prefix' => 'auth'], function () {
+    Route::post('/register', [AuthController::class, 'register']);
+    Route::post('/login', [AuthController::class, 'login']);
+});
