e1ba2a9ad1b0194c1aca541c5c551faffea612ed
mayra28mos <mayra28mos@gmail.com>
2023-04-16 16:18:13 +0200

    añado tabla orden

---
+++ backend/database/migrations/2023_04_16_134631_create_orders_table.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('orders', function (Blueprint $table) {
+            $table->id();            
+            $table->timestamps();
+            $table->unsignedBigInteger('costumerId')
+                ->references('id')
+                ->on('costumers')->onDelete('cascade');
+            $table->unsignedBigInteger('sellerId')
+                ->references('id')            
+                ->on('sellers')->onDelete('cascade');    
+            $table->string('type');
+            $table->date('date');
+            $table->integer('total');
+            $table->integer('invoiceNumber');
+            $table->string('ShippingAddress');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('orders');
+    }
+};
