6e8d0b013716ee25ad2db1d2f45ccbc48c9d1679
mayra28mos <mayra28mos@gmail.com>
2023-04-16 16:18:30 +0200

    añado tabla detalles de las ordenes

---
+++ backend/database/migrations/2023_04_16_135948_create_order_details_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('order_details', function (Blueprint $table) {           
+            $table->unsignedBigInteger('OrderId')
+                ->references('id')
+                ->on('orders')->onDelete('cascade');
+
+            $table->unsignedBigInteger('productId')
+                ->references('id')
+                ->on('products')->onDelete('cascade');
+                
+            $table->integer('quantity');
+            $table->integer('initePrice');
+            $table->integer('subTotal');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('order_details');
+    }
+};
