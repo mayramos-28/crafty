99b9d6cc08e3f17178a0fc0ade9db7c36db85630
mayra28mos <mayra28mos@gmail.com>
2023-04-17 17:19:24 +0200

    añadido el grupo de ruta de productos

--- backend/routes/api.php
+++ backend/routes/api.php
@@ -1,6 +1,7 @@
 <?php
 
 use App\Http\Controllers\AuthController;
+use App\Http\Controllers\ProductController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
@@ -24,4 +25,12 @@ Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
 Route::group(['prefix' => 'auth'], function () {
     Route::post('/register', [AuthController::class, 'register']);
     Route::post('/login', [AuthController::class, 'login']);
+});
+
+Route::group(['prefix'=> 'product'], function(){
+    Route::get('/index', [ProductController::class, 'index']);
+    Route::get('/show/{id}', [ProductController::class, 'show']);
+    Route::post('/store', [ProductController::class, 'store']);
+    Route::put('/update/{id}', [ProductController::class, 'update']);
+    Route::delete('/delete/{id}', [ProductController::class, 'destroy']);
 });
