e8d175afc450b5ba24cabf993f90391638185954
mayra28mos <mayra28mos@gmail.com>
2023-04-23 19:13:15 +0200

    añadido grupo rutas category

--- backend/routes/api.php
+++ backend/routes/api.php
@@ -1,6 +1,7 @@
 <?php
 
 use App\Http\Controllers\AuthController;
+use App\Http\Controllers\CategoryController;
 use App\Http\Controllers\ProductController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
@@ -16,11 +17,9 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
-    return $request->user();
- });
-
-//Route::post('/register', 'App\Http\Controllers\RegisterController@register');
+// Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
+//     return $request->user();
+//  });
 
 Route::group(['prefix' => 'auth'], function () {
     Route::post('/register', [AuthController::class, 'register']);
@@ -33,4 +32,12 @@ Route::group(['prefix'=> 'product'], function(){
     Route::post('/store', [ProductController::class, 'store']);
     Route::put('/update/{id}', [ProductController::class, 'update']);
     Route::delete('/delete/{id}', [ProductController::class, 'destroy']);
+});
+
+Route::group(['prefix'=> 'category'], function(){
+    Route::get('/index', [CategoryController::class, 'index']);
+    Route::get('/show/{id}', [CategoryController::class, 'show']);
+    Route::post('/store', [CategoryControllerer::class, 'store']);
+    Route::put('/update/{id}', [CategoryController::class, 'update']);
+    Route::delete('/delete/{id}', [CategoryController::class, 'destroy']);
 });
