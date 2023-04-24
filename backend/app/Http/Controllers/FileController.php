4c0b383e5d7ea588d0eeaa03015f4a95546dce74
mayra28mos <mayra28mos@gmail.com>
2023-04-24 12:46:38 +0200

    add FIle controller

---
+++ backend/app/Http/Controllers/FileController.php
@@ -0,0 +1,51 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\File;
+use Illuminate\Http\Request;
+use Illuminate\Routing\Controller;
+
+class FileController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(string $id)
+    {
+        $file = File::find($id);
+        return response()->redirectTo($file->attached);
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(Request $request, string $id)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(string $id)
+    {
+        //
+    }
+}
