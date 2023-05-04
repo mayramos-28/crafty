820975423ce3ccdddda7834cd5c5beb9117775bf
mayra28mos <mayra28mos@gmail.com>
2023-05-04 21:53:36 +0200

    nuevo componente FooterLayout

---
+++ frontend/src/layout/FooterLayout.jsx
@@ -0,0 +1,17 @@
+export const FooterLayout = () => {
+    return (
+        <footer className=" bg-dark-subtle bg-opacity-75 d-flex justify-content-center align-items-end"  >
+            <div className="text-center fw-bolder">
+                <em className="fs-4">© 2023 Copyright;</em><span className="fs-3"> Crafty.SL</span>
+                <div className="">
+                    <ul className="d-flex justify-content-between gap-5 ">
+                        <li><a href="#" className="link-offset-3 link-underline link-underline-opacity-0">Lorem ipsum dolor sit amet.</a></li>
+                        <li><a href="#" className="link-offset-3 link-underline link-underline-opacity-0">Lorem ipsum dolor sit amet.</a></li>
+                        <li><a href="#" className="link-offset-3 link-underline link-underline-opacity-0">Lorem ipsum dolor sit amet.</a></li>
+                        <li><a href="#" className="link-offset-3 link-underline link-underline-opacity-0">Lorem ipsum dolor sit amet.</a></li>
+                    </ul>
+                </div>
+            </div>
+        </footer>
+    )
+}
