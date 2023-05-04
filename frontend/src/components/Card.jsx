c7d96e89eff18b39cd45fe26ff7a3c6495a5d0c3
mayra28mos <mayra28mos@gmail.com>
2023-05-04 21:50:57 +0200

    nuevo CardComponent

---
+++ frontend/src/components/Card.jsx
@@ -0,0 +1,24 @@
+import { craftyFileUrl } from "../api/FileApi";
+import FirsetImage from "./../assets/image/carousel/modelado-alta-vista-arcilla-torno-alfarero.jpg";
+import Card from 'react-bootstrap/Card';
+
+
+
+export const CardComponent = ({ props, linkTo }) => {
+
+    return (        
+        <>
+        <Card style={{ width: '18rem' }} className=" col-lg-4 col-md-2 col-sm-12 my-2">
+            <Card.Img variant="top" src={craftyFileUrl(props.imageId)} onError={(e) => { e.target.src = FirsetImage }} style={{ maxHeight: '178px' }} className="py-2" />
+            <Card.Body>
+                <Card.Title className="d-flex justify-content-center">{props.name}</Card.Title>
+                <Card.Text>
+                {props.description}
+                </Card.Text>
+                <Card.Link href={linkTo}  className="d-flex justify-content-center fs-4" >Ver {props.name}</Card.Link>                
+            </Card.Body>
+        </Card>
+
+        </>
+    );
+};
