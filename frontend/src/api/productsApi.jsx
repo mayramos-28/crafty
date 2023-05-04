046e56b27a8a9d86db7019158a0f80551036f596
mayra28mos <mayra28mos@gmail.com>
2023-05-04 21:50:12 +0200

    mejora llamdas Api productos

--- frontend/src/api/productsApi.jsx
+++ frontend/src/api/productsApi.jsx
@@ -1,19 +1,29 @@
 import { craftyApi } from "./craftyApi";
 
 
-export const getProducts = async() => {
+export const getProducts = async(categoryId) => {
      const response = await craftyApi({
         method: 'GET',
-        uri: 'products/index'
+        uri: 'product/index',
+        query: {categoryId}
+    });
+
+    return response.data;
+    }
+export const showProduct = async(productId) => {
+    const response = await craftyApi({
+        method: 'GET',
+        uri: `product/show/${productId}`
+     
     });
 
     return response.data;
     }
 
-export const createProduct = async(product) => {
+export const createProduct = async(newProduct) => {
     const response = await craftyApi({
         method: 'POST',
-        uri: 'products/store',
-        body: product
+        uri: 'product/store',
+        body: newProduct
     });
 }
