f0aa49f9b1e7059ad02d41fcfde4d8a481d03f7d
mayra28mos <mayra28mos@gmail.com>
2023-05-04 21:48:06 +0200

    Nuevo esqueleto para hacer llamadas a la api

--- frontend/src/api/craftyApi.jsx
+++ frontend/src/api/craftyApi.jsx
@@ -1,11 +1,13 @@
 
-const url = 'http://localhost:8080/api';
+export const CRAFTY_BASE_URL = 'http://localhost:8080/api';
 
 
 export const craftyApi = async (
-    { method, uri, body }
-) => { 
-    const response = await fetch(`${url}/${uri}`, {
+    { method, uri, body, query }
+) => {
+    const endpoint = `${CRAFTY_BASE_URL}/${uri}` + (query ? `?${new URLSearchParams(query)}` : '');
+
+    const response = await fetch(endpoint, {
         method: method,
         headers: {
             'Content-Type': 'application/json'
@@ -13,7 +15,7 @@ export const craftyApi = async (
         body: typeof body === 'object' ? JSON.stringify(body) : body
     });
     const data = await response.json();
-    if(response.status !== 200){
+    if (response.status  < 200 || response.status >= 300) {
         throw new Error(data.message);
     }
     return data;
