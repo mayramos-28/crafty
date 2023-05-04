6982f34d1c03af42d0054910225cdef66651e5b7
mayra28mos <mayra28mos@gmail.com>
2023-05-04 21:56:04 +0200

    nuevos reducers

--- frontend/src/store/craftyStore.jsx
+++ frontend/src/store/craftyStore.jsx
@@ -1,11 +1,18 @@
 import { configureStore } from "@reduxjs/toolkit";
 import { registerUserSlice } from "./slices/register";
 import { loginUsersSlice } from "./slices/login";
+import { CategoryReducer } from "./slices/categorySlice";
+import logger from 'redux-logger'
+import { ProductReducer } from "./slices/ProductSlice";
+import { authUserSlice } from "./slices/authUserSlice";
 
 export const store = configureStore({
     reducer: {
         register: registerUserSlice.reducer,
-        login: loginUsersSlice.reducer,
-       
+        login: loginUsersSlice.reducer,        
+        category: CategoryReducer,
+        product: ProductReducer,
+        authUser: authUserSlice.reducer
     },
+    middleware: (getDefaultMiddleware) => getDefaultMiddleware().concat(logger),
 });
