a2dcfa421aef7155f0d2dcb97da1acdbf4d88d48
mayra28mos <mayra28mos@gmail.com>
2023-05-04 21:55:13 +0200

    nuevo componente para zona perfil

---
+++ frontend/src/page/ProfilePage.jsx
@@ -0,0 +1,45 @@
+import { useEffect, useRef } from "react";
+import { useDispatch, useSelector } from "react-redux";
+import { fetchAuthUser } from "../store/slices/authUserSlice";
+import { ConsumerPage } from "./ConsumerPage";
+
+import Nav from 'react-bootstrap/Nav';
+import { NavLink, Navigate, Outlet, Route } from "react-router-dom";
+
+
+export const ProfilePage = () => {
+   
+    const isLogged = localStorage.getItem("token") !== null;
+
+    if(!isLogged) return (
+        
+           <Navigate to="/" />
+        
+    )
+  
+    return (
+        <div className="loginPage">
+           
+            <Nav variant="tabs" defaultActiveKey="/myArea">
+                <Nav.Item>
+                    <Nav.Link as={NavLink} to="/profile/my-area">Mi informacion</Nav.Link>
+                </Nav.Item>
+                <Nav.Item>
+                    <Nav.Link as={NavLink} to="/profile/shopping">Mi area de compras</Nav.Link>
+                </Nav.Item>
+                <Nav.Item>
+                    <Nav.Link as={NavLink} to="/profile/sales">Mi area de ventas
+                    </Nav.Link>
+                </Nav.Item>
+            </Nav>
+          
+
+            <section className="h-90">
+                
+                <Outlet></Outlet>
+            </section>
+
+
+        </div>
+    );
+}
