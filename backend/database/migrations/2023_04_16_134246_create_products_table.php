4227749ae29dbcc4ef73082cad582e902acca222
mayra28mos <mayra28mos@gmail.com>
2023-05-06 21:33:18 +0200

    Nuevo campo rating

--- backend/database/migrations/2023_04_16_134246_create_products_table.php
+++ backend/database/migrations/2023_04_16_134246_create_products_table.php
@@ -17,6 +17,7 @@ return new class extends Migration
             $table->string('description');       
             $table->integer('price');
             $table->integer('stock')->nullable();
+            $table->integer('rating')->nullable();
             $table->unsignedBigInteger('categoryId')
             ->references('id')
             ->on('categories')
