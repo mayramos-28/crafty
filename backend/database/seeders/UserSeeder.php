93186ef8c651f575fd0557e6954c0fe1c064b395
mayra28mos <mayra28mos@gmail.com>
2023-05-08 20:01:03 +0200

    nuevo semillero user

--- backend/database/seeders/UserSeeder.php
+++ backend/database/seeders/UserSeeder.php
@@ -6,7 +6,7 @@ use App\Models\User;
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
 
-class CostumerSeeder extends Seeder
+class UserSeeder extends Seeder
 {
     /**
      * Run the database seeds.
