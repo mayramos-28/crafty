0aad0a02453daaffb96e32b5806af57b9ffcbfd3
mayra28mos <mayra28mos@gmail.com>
2023-05-08 20:00:53 +0200

    userSeeder añadido

--- backend/database/seeders/DatabaseSeeder.php
+++ backend/database/seeders/DatabaseSeeder.php
@@ -4,6 +4,7 @@ namespace Database\Seeders;
 
 // use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 
+use App\Models\User;
 use Illuminate\Database\Seeder;
 
 class DatabaseSeeder extends Seeder
@@ -14,7 +15,8 @@ class DatabaseSeeder extends Seeder
     public function run(): void
     {
         $this->call([
-            SellerSeeder::class,           
+            SellerSeeder::class,  
+            UserSeeder::class,         
             AddressSeeder::class,
             CategoriesSeeder::class,     
             OrderTypesSeeder::class,
