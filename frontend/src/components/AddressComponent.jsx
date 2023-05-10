196ae1a80bf983f6213f503a359db4955891a46e
mayra28mos <mayra28mos@gmail.com>
2023-05-10 19:25:20 +0200

    Modificacion filtros direcciones

--- frontend/src/components/AddressComponent.jsx
+++ frontend/src/components/AddressComponent.jsx
@@ -2,63 +2,55 @@ import { useDispatch, useSelector } from "react-redux";
 import { fetchAddress, getAddress, selectAddressError, selectAddressLoading, selectAllAddress } from "../store/slices/AddressSlice";
 import { useEffect, useRef, useState } from "react";
 
-export const AddressComponent = ({userId}) => {
+export const AddressComponent = ({ userId }) => {
 
-    const firstExecution = useRef(true);
-    const address = useSelector(selectAllAddress);
-    const error = useSelector(selectAddressError);
-    const loading = useSelector(selectAddressLoading);
+  const firstExecution = useRef(true);
+  const address = useSelector(selectAllAddress);
+  const error = useSelector(selectAddressError);
+  const loading = useSelector(selectAddressLoading);
 
-   
-    const dispatch = useDispatch();
 
-    useEffect(() => {
-        if (firstExecution.current) {
-        
-            dispatch(fetchAddress({userId}));
-            firstExecution.current = false;
-        }
-    }, [dispatch, firstExecution]);
-    console.log('Address',address,'userId', userId);
+  const dispatch = useDispatch();
 
-console.log('Direcciones',address,'userId', userId);
-    return (
-        <div>
-            <h1>Mis Direcciones</h1>
-            <div>
-            <ul>
-  {Object.keys(address ?? {}).map((key) => {
-    if (typeof address[key] === 'object' && address[key] !== null) {
-      return (
-        <li key={key}>
-          {key}:
-          <ul>
-            {Object.keys(address[key]).map((innerKey) => (
-              <li key={innerKey}>
-                {innerKey}: {address[key][innerKey]}
-              </li>
-            ))}
-          </ul>
-        </li>
-      );
-    } else {
-      return (
-        <li key={key}>
-          {key}: {address[key]}
-        </li>
-      );
+  useEffect(() => {
+    if (firstExecution.current) {
+
+      dispatch(fetchAddress({ userId }));
+      firstExecution.current = false;
     }
-  })}
-</ul>
+  }, [dispatch, firstExecution]);
+  console.log('Address', address, 'userId', userId);
 
-
-           
-            
-
-            </div>
-
-        
-
-        </div>
-    )
+  console.log('Direcciones', address, 'userId', userId);
+  return (
+    <div>
+      <h1>Mis Direcciones</h1>
+      <div>
+        <ul>
+          {Object.keys(address ?? {}).map((key) => {
+            if (typeof address[key] === 'object' && address[key] !== null) {
+              return (
+                <li key={key}>
+                  {key}:
+                  <ul>
+                    {Object.keys(address[key]).map((innerKey) => (
+                      <li key={innerKey}>
+                        {innerKey}: {address[key][innerKey]}
+                      </li>
+                    ))}
+                  </ul>
+                </li>
+              );
+            } else {
+              return (
+                <li key={key}>
+                  {key}: {address[key]}
+                </li>
+              );
+            }
+          })}
+        </ul>
+      </div>
+    </div>
+  )
 };
