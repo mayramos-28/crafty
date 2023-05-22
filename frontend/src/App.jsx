6c0fe30cc61edbf0567442c130f6e2c2a9080fa2
mayra28mos <mayra28mos@gmail.com>
2023-05-22 21:37:41 +0200

    modificaciones descartadas

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,11 +1,29 @@
-import { RegistrationPage } from "./components/forms/RegistrationPage";
+
 import "bootstrap/dist/css/bootstrap.min.css";
-import "./assets/style/main.css";
-import "./assets/style/categoryBackground.css";
+import "bootstrap-icons/font/bootstrap-icons.css";
+import "./assets/style/index.css";
 import { MainLayout } from "./layout/MainLayout";
 import { FooterLayout } from "./layout/FooterLayout";
+import { useEffect } from "react";
+import { useDispatch, useSelector } from "react-redux";
+
 
 function App() {
+
+  // const cartItems = useSelector((state) => state.cart.cartItems);
+
+  // useEffect(() => {
+  //   window.addEventListener("beforeunload", handleBeforeUnload);
+
+  //   return () => {
+  //     window.removeEventListener("beforeunload", handleBeforeUnload);
+  //   };
+  // }, []);
+
+  // const handleBeforeUnload = () => {
+  //   localStorage.setItem("cartItems", JSON.stringify(cartItems));
+  // };
+
   return (
     <>
       <div className="App d-flex flex-column">
