00bea5d6b3301e1e4aabf179bfac0d738006c1c2
mayra28mos <mayra28mos@gmail.com>
2023-05-22 21:40:09 +0200

    Nueva pagina de productos

---
+++ frontend/src/page/ProductsPage.jsx
@@ -0,0 +1,58 @@
+import { MainCarousel } from "../components/MainCarousel";
+import { useDispatch, useSelector } from "react-redux";
+import { useEffect, useRef, useState } from "react";
+import { fetchProducts, selectAllProducts, selectProductError, selectProductLoading } from "../store/slices/ProductSlice";
+import { Spinner } from "react-bootstrap";
+import { useSearchParams } from "react-router-dom";
+import { CardProductComponent } from "../components/CardProductComponete";
+import { loadCartItems } from "../store/slices/CartSlice";
+
+
+export const ProductsPage = () => {
+  
+    const Products = useSelector(selectAllProducts);
+    const error = useSelector(selectProductError);
+    const isLoading = useSelector(selectProductLoading);
+    const dispatch = useDispatch();
+    const firstExecution = useRef(true);
+    const [searchParams, setSearchParams] = useSearchParams();
+    const categoryId = searchParams.get("categoryId");
+   
+    const cartItems = useSelector((state) => state.cart.cartItems);
+
+    useEffect(() => {
+        if (firstExecution.current) {
+            dispatch(fetchProducts({categoryId}))
+            dispatch(loadCartItems());
+            firstExecution.current = false;
+        }
+    }, [dispatch, firstExecution]);
+
+     
+
+    return (
+        <>
+            <div className="mainPage  ">
+                
+                <div className="mainPage__carousel">
+                    <MainCarousel />
+
+                </div>
+               
+
+                <div className="align-product">
+                    <div className="mainProducts d-flex justify-content-around py-4 px-4 row gap-2">
+                        {
+                            Products.map((product) => (<CardProductComponent key={product.id} props={product}  linkTo={`/products/${product.id}`}  ></CardProductComponent>))
+                        }
+                    </div>
+                </div>
+                {isLoading && <Spinner />}
+                {error && <div className="alert alert-danger">{error}</div>}                
+            </div>
+
+        </>
+
+    )
+}
+    ;
