3e46011dfdf04bd3b1da7e3740b6d6ae62bd7ba5
mayra28mos <mayra28mos@gmail.com>
2023-05-22 21:28:23 +0200

    archivos modificados o eliminados

--- backend/app/Models/sellerWithdrawalAccount.php
+++ backend/app/Models/sellerWithdrawalAccount.php
@@ -5,7 +5,7 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
-class selleWithdrawalAccount extends Model
+class sellerWithdrawalAccount extends Model
 {
     use HasFactory;
 
