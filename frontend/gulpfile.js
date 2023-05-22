a90b20a329ccd0671d83358f273ec0bda478d0e7
mayra28mos <mayra28mos@gmail.com>
2023-05-22 21:33:17 +0200

    tarea gulp creada

---
+++ frontend/gulpfile.js
@@ -0,0 +1,24 @@
+const gulp = require('gulp');
+const sass = require('gulp-sass')(require('sass'));
+const cleanCSS = require('gulp-clean-css');
+
+gulp.task('sass', function(cb) {
+  gulp
+    .src('./src/assets/style/index.scss')
+    .pipe(sass())
+    .pipe(cleanCSS({compatibility: 'ie8'}))
+    .pipe(
+      gulp.dest(function(f) {
+        return f.base;
+      })
+    );
+  cb();
+});
+
+gulp.task(
+  'default',
+  gulp.series('sass', function(cb) {
+    gulp.watch('./**/*.scss', gulp.series('sass'));
+    cb();
+  })
+);
