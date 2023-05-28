716b4483daef0980cb5966a7a26d89aaead5161d
mayra28mos <mayra28mos@gmail.com>
2023-05-28 07:52:21 +0200

    cambio mayusculas 270523

--- backend/app/Models/Seller.php
+++ backend/app/Models/Seller.php
@@ -11,9 +11,9 @@ class Seller extends Model
 
     protected $guarded = [];
 
-    public function User()
+    public function user()
     {
-        return $this->belongsTo(User::class);
+        return $this->belongsTo(User::class, 'userId');
     }
 
    public function sellerWithdrawalAccount()
