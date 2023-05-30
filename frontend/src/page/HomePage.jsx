1d6d92d5d310864bf467e14147306da6a9d04587
mayra28mos <mayra28mos@gmail.com>
2023-05-30 12:25:55 +0200

    modificado texto pag inicial

--- frontend/src/page/HomePage.jsx
+++ frontend/src/page/HomePage.jsx
@@ -3,7 +3,8 @@ import { CardComponent as CategoryCard } from "../components/Card";
 import { useEffect, useState } from "react";
 import { fetchCategories, selectAllCategories, selectCategoryError, selectCategoryLoading } from "../store/slices/categorySlice";
 import { MainCarousel } from "../components/MainCarousel";
-import { Col, Row } from "react-bootstrap";
+import { Col, Nav, Row } from "react-bootstrap";
+import { Outlet } from "react-router-dom";
 
 
 export const HomePage = () => {
@@ -24,28 +25,54 @@ export const HomePage = () => {
 
 
 
+
     return (
-
-        <>
-            <Row >
-                <Col md={12}> <MainCarousel /> </Col>
-                <Col>
-                    <Row className="gap-3 py-5 d-flex justify-content-center">
-                        <h2>Categorias</h2>
-
-
-                        {categories.map((category) => (
-                            <Col sm={11} md={4} lg={3} className="d-flex justify-content-center">
-                                <CategoryCard key={category.id} props={category} linkTo={`/products?categoryId=${category.id}`}></CategoryCard>
-                            </Col>
-                        ))
-                        }
-
-                    </Row>
-
-                </Col>
-            </Row>
-
-        </>
-    );
+        <div className="homepage d-flex flex-column justify-content-center">
+          <h1 className="homepage-title">¡Bienvenido a Crafty!</h1>
+          <p className="homepage-description">
+            Tu lugar de encuentro para artículos artesanales únicos
+          </p>
+      
+          <div className="homepage-highlights">
+            <div className="highlight">
+              {/* Imagen y contenido de "Amplia variedad de productos" */}
+            </div>
+      
+            <div className="highlight">
+              {/* Imagen y contenido de "Vendedores confiables" */}
+            </div>
+      
+            <div className="highlight">
+              {/* Imagen y contenido de "Gestión segura de pagos y pedidos" */}
+            </div>
+      
+            <div className="highlight">
+              {/* Imagen y contenido de "Almacenamiento y envío confiables" */}
+            </div>
+          </div>
+      
+          <div className="homepage-cta">
+            <h2 className="cta-title">¡Únete a nuestra comunidad!</h2>
+            <p className="cta-description">
+              Ya sea que estés buscando un regalo único o quieras compartir tus creaciones con el mundo,
+              Crafty es el lugar ideal para ti. Explora nuestro marketplace, descubre nuevas pasiones y
+              sumérgete en el maravilloso mundo de la artesanía.
+            </p>
+          </div>
+      
+          <div className="homepage-contact">
+            <h2 className="contact-title">¡Contáctanos!</h2>
+            <p className="contact-description">
+              Estamos aquí para ayudarte. Si tienes alguna pregunta, no dudes en ponerte en contacto con
+              nuestro equipo de atención al cliente.
+            </p>
+            <div className="contact-info">
+              <p>Teléfono: +1 123-456-7890</p>
+              <p>Correo electrónico: info@crafty.com</p>
+              <p>Dirección: Calle Artesanía 123, Ciudad Artesana, País Artesanía</p>
+            </div>
+          </div>
+        </div>
+      );
+      
 }
